c6a7a6d477c9f386077919dce75fdcb61334dbda
anonymouy <tangyn23@mail2.sysu.edu.cn>
2025-04-02 16:09:44 +0800

    submit

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -1,3 +1,4 @@
+
 // strings2.rs
 //
 // Make me compile without changing the function signature!
@@ -5,11 +6,11 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
